6090b0d7f4a90c71c16368c0f784a827735dc6ea
martij62 <37908290+martij62@users.noreply.github.com>
2018-04-12 11:14:54 -0500

    Add files via upload

---
+++ Activities/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Lab15.Resources.layout;
+
+namespace Lab15
+{
+    [Activity(Label = "Book Inventory", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        // Step 2
+        EditText txtEmail;
+        EditText txtPassword;
+        Button btnLogin;
+        Button btnRegister;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        
+            // Step 1
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Step 3
+            txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+
+            // Step 4
+            btnLogin.Click += btnLogin_Click;
+            btnRegister.Click += btnRegister_Click;
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+    }
+}
+
---
+++ Activities/RegisterActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Lab15.Resources.layout
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+
+        EditText txtEmail;
+        EditText txtPassword;
+        Button btnRegister;
+        EditText txtConfirmPassword;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Register);
+
+            txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+            txtConfirmPassword = FindViewById<EditText>(Resource.Id.txtConfirmPassword);
+        }
+    }
+}
